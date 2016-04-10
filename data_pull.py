0a3d5bdb3d72723632f032af22a076d993cae0ca
Ubuntu <ubuntu@ip-172-31-28-177.us-west-2.compute.internal>
2016-04-10 04:51:57 +0000

    Second commit

--- data_pull.py
+++ data_pull.py
@@ -19,7 +19,23 @@ vi_stackexchange_post_links = pd.read_sql_query('SELECT * FROM vi_stackexchange_
 vi_stackexchange_posts_history = pd.read_sql_query('SELECT * FROM vi_stackexchange_posts_history', engine)
 vi_stackexchange_tags = pd.read_sql_query('SELECT * FROM vi_stackexchange_tags', engine)
 vi_stackexchange_users = pd.read_sql_query('SELECT * FROM vi_stackexchange_users', engine)
-vi_stackexchange_votes = pd.read_sql_query('SELECT * FROM vi_stackexchange_votes', engine)
+vi_stackexchange_good_votes = pd.read_sql_query('SELECT * FROM vi_stackexchange_votes where VoteTypeId in (1,2,5)', engine)
+vi_stackexchange_bad_votes = pd.read_sql_query('SELECT * FROM vi_stackexchange_votes where VoteTypeId in (3,4,12)', engine)
 
 
-         
+vi_stackexchange_votes_v1 = vi_stackexchange_good_votes[['Id', 'PostId', 'VoteTypeId']]
+vi_stackexchange_votes_v1 = vi_stackexchange_votes_v1.groupby(['PostId'])
+vi_stackexchange_votes_v1 = vi_stackexchange_votes_v1['VoteTypeId'].count()
+vi_stackexchange_votes_v1.rename(columns={'count':'total_good'})
+vi_stackexchange_votes_v1 = vi_stackexchange_votes_v1.to_frame()
+
+vi_stackexchange_votes_v2 = vi_stackexchange_bad_votes[['Id', 'PostId', 'VoteTypeId']]
+vi_stackexchange_votes_v2 = vi_stackexchange_votes_v2.groupby(['PostId'])
+vi_stackexchange_votes_v2 = vi_stackexchange_votes_v2['VoteTypeId'].count()
+vi_stackexchange_votes_v2.rename(columns={'count':'total_bad'})
+vi_stackexchange_votes_v2 = vi_stackexchange_votes_v2.to_frame()
+
+#vi_stackexchange_votes_v3 = vi_stackexchange_votes_v1.merge(vi_stackexchange_votes_v2, how='outer', left_on = 'PostId', right_on = 'PostId' )
+print vi_stackexchange_votes_v1
+#print vi_stackexchange_votes_v3
+
